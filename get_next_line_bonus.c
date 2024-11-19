50559aff4a8d9d19dc1408f039019b0e82399fb2
Leo Polizzi <lpolizzi@c1r3p9.42nice.fr>
2024-11-19 11:33:11 +0100

    Removed usefull test because moulinette is stupid ffs.

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -95,7 +95,7 @@ char	*get_next_line(int fd)
 	static char	*data[MAX_FD];
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
 		if (data[fd])
 		{
